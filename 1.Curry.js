a4e33517aeafdfebd72484520720830fbbc55c36
mdazadabdullah <jobs4angular@gmail.com>
2024-08-14 14:35:47 -0700

    currying done !

---
+++ 1.Curry.js
@@ -0,0 +1,40 @@
+// Currying is a useful technique used in JavaScript applications.
+
+// Please implement a curry() function, which accepts a function and return a curried one.
+
+// Here is an example
+
+// const join = (a, b, c) => {
+//    return `${a}_${b}_${c}`
+// }
+
+// const curriedJoin = curry(join)
+// curriedJoin(1, 2, 3) // '1_2_3'
+// curriedJoin(1)(2, 3) // '1_2_3'
+// curriedJoin(1, 2)(3) // '1_2_3'
+
+
+// <!---------------SOLUTION:----------->
+
+function curry(fn) {
+    return function curried(...args) {
+      if(args.length >= fn.length) {
+        return fn.apply(this, args);
+      } else {
+        return function (...args2) {
+          return curried.apply(this, args.concat(args2));
+        }
+      }
+    }
+  }
+  const join = (a, b, c) => {
+     return `${a}_${b}_${c}`
+  }
+  const curriedJoin = curry(join)
+  curriedJoin(1, 2, 3) // '1_2_3'
+  curriedJoin(1)(2, 3) // '1_2_3'
+  curriedJoin(1, 2)(3) // '1_2_3'
+  
+  console.log(curriedJoin(1, 2, 3));
+  console.log(curriedJoin(1)(2, 3));
+  console.log(curriedJoin(1)(2)(3));
